c91db40d9a306159788c277e693de96c9479a2db
root <root@master.inf.puppetlabs.demo>
2015-04-24 21:54:52 +0000

    added firewall rules and removed vagrant 127.0.1.1 line from hosts

--- manifests/config.pp
+++ manifests/config.pp
@@ -28,4 +28,19 @@ class razordemo::config (
     outiface => 'eth0',
     table    => 'nat',
   }
+  firewall { '150 Allow DHCP and TFTP':
+    proto  => 'udp',
+    port   => [67, 68, 69],
+    action => 'accept',
+  }
+  firewall { '175 Allow Razor Ports':
+   proto  => ['udp','tcp'],
+   port   => [8150, 8151],
+   action => 'accept',
+  }
+  file_line { '/etc/hosts':
+    ensure => 'absent',
+    path   => '/etc/hosts',
+    line   => "127.0.1.1 $::fqdn $::hostname"
+  }
 }
