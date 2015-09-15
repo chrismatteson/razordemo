5d9d77509c51e1da1bc7e8150fe1be1ca792ab78
root <root@master.inf.puppetlabs.demo>
2015-09-15 23:35:47 +0000

    Remove implementation specific resources (razor repos and policies) from module

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,22 +1,14 @@
 class razordemo::config (
-  $repos,
   $brokers,
   $tasks,
-  $policies,
   $tags
   ) {
 
-  unless $repos == 'undef' {
-    create_resources('razor_repo',$repos)
-  }
   unless $brokers == 'undef' {
     create_resources('razor_broker',$brokers)
   }
   unless $tasks == 'undef' {
     create_resources('razor_task',$tasks)
-  }
-  unless $policies == 'undef' {
-    create_resources('razor_policy',$policies)
   }
   unless $tags == 'undef' {
     create_resources('razor_tag',$tags)
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,19 +1,6 @@
-class razordemo::params (
-  $dnsmasq_config_dir  = '/etc/dnsmasq.d',
-  $dnsmasq_config_file = '/etc/dnsmasq.conf',
-  $repos = {
-    'centos-6.7' => {
-      'ensure'  => 'present',
-      'iso_url' => 'https://s3-us-west-2.amazonaws.com/tse-builds/razor-images/CentOS-6.7-x86_64-minimal.iso',
-      'task'    => 'centos/6',
-    },
-#    'esxi-5.5' => {
-#      'ensure'  => 'present',
-#      'iso_url' => 'https://s3-us-west-2.amazonaws.com/tse-builds/razor-images/VMware-VMvisor-Installer-5.5.0-1331820.x86_64.iso',
-#      'task'    => 'vmware_esxi',
-#    },
-
-  },
+class razordemo::params {
+  $dnsmasq_config_dir  = '/etc/dnsmasq.d'
+  $dnsmasq_config_file = '/etc/dnsmasq.conf'
   $brokers = {
     'puppet-enterprise' => {
       'ensure'      => 'present',
@@ -22,37 +9,12 @@ class razordemo::params (
         'server' => 'master.inf.puppetlabs.demo',
       },
     }
-  },
-  $policies = {
-    'centos-6.7-for-virtual' => {
-      'ensure'        => 'present',
-      'repo'          => 'centos-6.7',
-      'task'          => 'centos/6',
-      'broker'        => 'puppet-enterprise',
-      'hostname'      => 'host${id}',
-      'root_password' => 'puppet',
-      'max_count'     => 20,
-      'node_metadata' => {},
-      'tags'          => ['virtual'],
-    },
-#    'esxi-5.5-for-virtual' => {
-#      'ensure'        => 'present',
-#      'repo'          => 'esxi-5.5',
-#      'task'          => 'vmware_esxi',
-#      'broker'        => 'puppet-enterprise',
-#      'hostname'      => 'host${id}',
-#      'root_password' => 'puppet',
-#      'max_count'     => 20,
-#      'node_metadata' => {},
-#      'tags'          => ['virtual'],
-#    }
-  },
+  }
   $tags = {
     'virtual' => {
       'ensure' => 'present',
       'rule'   => ['=', ['fact', 'is_virtual'], true ],
     }
-  },
+  }
   $tasks = 'undef'
-  ){
 }
