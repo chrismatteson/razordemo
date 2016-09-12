14152547010f1753c6805a35a3183fbeaa7873ee
tse-puppetmaster1-prod <N/A>
2016-09-12 02:50:21 +0000

    added esxi broker

--- manifests/params.pp
+++ manifests/params.pp
@@ -9,6 +9,15 @@ class razordemo::params {
         'server' => 'master.inf.puppetlabs.demo',
         'windows_agent_download_url' => 'https://pm.puppetlabs.com/puppet-agent/2015.2.1/latest/repos/windows/puppet-agent-x64.msi',
       },
+    },
+    'esxi' => {
+      'ensure'      => 'present',
+      'broker_type' => 'esxi',
+      'configuration' => {
+        'vc_server' => 'tse-vc1-prod.corp.puppetlabs.net',
+        'vc_username' => 'tseadmin@vsphere.local',
+        'vc_password' => 'PuppetLabs=>10',
+      },
     }
   }
   $tags = {
